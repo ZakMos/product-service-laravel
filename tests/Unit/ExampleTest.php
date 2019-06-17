8a8007a4a4152f6bbf80e105a4be07d8d5d8cfce
ZakMos <z.zza@hotmail.com>
2019-06-17 16:58:24 +0200

    Creating Routs

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -4,6 +4,9 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
@@ -12,8 +15,14 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testBasicExample()
     {
-        $this->assertTrue(true);
+         $this->visit('/')
+              ->see('Laravel 5');
+    }
+    public function testBasicList()
+    {
+         $this->get(route('products'))
+              ->assertResponseOk();
     }
 }
